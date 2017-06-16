b884f7cc30373ee952c390b438c02bbc6f7e9a88
María José Rodríguez F <rodriguez.mariajose.f@gmail.com>
2017-06-16 14:37:33 -0400

    boton-js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 /*aquí va tu código*/
+
+
+function guardarDatos(){
+	localStorage.nombre = document.getElementById("nombre").value;
+	localStorage.comentarios = document.getElementById("comentarios")
+}
+
+
+
