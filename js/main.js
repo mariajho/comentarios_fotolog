f9b766e53556240ddb47705bfc684dcf84de9298
María José Rodríguez F <rodriguez.mariajose.f@gmail.com>
2017-06-18 23:45:12 -0400

    ejercicio-finalizado-faltan-detalles

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 /*funcion boton*/
 function init(){
 	var button = document.getElementById("boton");
-	button.addEventListener("click", guardar);
+	button.addEventListener("click", guardarDatos);
 	guardarDatos();
 }
 
@@ -15,7 +15,8 @@ function guardarDatos(){
 	localStorage.setItem(clave, valor);
 
 	/*limpia contenedor para no repetir valores en html*/
-	var contenedor.innerHTML="";
+	var contenedor = document.getElementById("ale");
+	contenedor.innerHTML="";
 
 	/*recorre los valores ingresados*/
 	for(var i = 0; i < localStorage.length; i++);
@@ -31,22 +32,18 @@ function guardarDatos(){
 	var titulo = document.createElement("h1");/*titulo es el padre*/
 	var nombre = document.createTextNode(kei);/*crea un nodo hijo*/
 	titulo.classList.add("datos-usuario");/*agrega una clase al padre*/
-	titulo.appendChild("nombre");/*le asigna un padre al nodo hijo*/
+	titulo.appendChild(nombre);/*le asigna un padre al nodo hijo*/
 
 	var parrafo = document.createElement("p");/*titulo es el padre*/
 	var comentario = document.createElement("values");/*nombre es el hijo*/
 	parrafo.classList.add("post");/*agrega una clase al padre*/
-	parrafo.appendChild("comentario");/*le asigna un padre al nodo hijo*/
+	parrafo.appendChild(comentario);/*le asigna un padre al nodo hijo*/
 
-
-
-
-
-
-
-
-
-
+/*Luego asignamos el padre(caja) a los hijos (nombre y comentarios) para guardarlos.*/
+	caja.appendChild(nombre);
+	caja.appendChild(comentario);
+/*finalmente le asignamos el padre (contenedor) al hijo (caja), donde me muestra todo el contenedor creado*/
+	contenedor.appendChild(caja);
 
 
 
@@ -57,6 +54,7 @@ function init(){
 }
 
 
-function recuperarDatos(){
-	if((localStor))
+function limpiarDatos(){
+	localStorage.clear();
+	guardarDatos();	
 }
